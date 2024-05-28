7224e3468a86ca1054b9f930540acfe3b86431cd
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-05-28 09:43:50 -0400

    More type guard

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 type Admin = {
-    name: string;
-    privileges: string[];
+  name: string;
+  privileges: string[];
 };
 
 type Employee = {
-    name: string;
-    startDate: Date;
+  name: string;
+  startDate: Date;
 };
 
 // interface ElevatedEmployee extends Employee, Admin {}
@@ -13,25 +13,64 @@ type Employee = {
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date()
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
 };
 
 type Combinable = string | number;
 type Numeric = number | boolean;
 
-type Universal = Combinable & Numeric; 
+type Universal = Combinable & Numeric;
 
 function add(a: Combinable, b: Combinable) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
 type UnknownEmployee = Employee | Admin;
 
 function printEmployeeInformation(emp: UnknownEmployee) {
-    console.log('Name' + emp.name);
+  console.log("Name" + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges" + emp.privileges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Start Date" + emp.startDate);
+  }
 }
+
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading Cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
